05e743cfe538477547f8d8dd866e077af9b15bca
sekugr <sekugr@gmail.com>
2018-11-21 19:39:39 +0300

    добавлена task 10

---
+++ task10/task/task_1.cpp
@@ -0,0 +1,39 @@
+
+
+#include "stdafx.h"
+#include "task.h"
+#include <stack>
+
+
+
+using namespace std;
+
+
+void task_1()
+{
+	cout << "Задание 10.1" << endl;
+	stack <char> st;
+
+	int size;
+	char str[80];// = { "Какаято строка" };
+	cout << "Введи строку \n";
+	cin.getline(str, 80);
+	clr_cin;
+	size = strlen(str);
+	
+	//cout << size;
+	for (int i = 0; i < size; i++)
+	{
+		st.push(str[i]);
+	}
+	for (int i = 0; i < size ; i++)
+	{
+		str[i] = st.top();
+		st.pop();
+	}
+
+	
+	
+	
+	cout << str << endl;
+}
---
+++ task10/task/task_3.cpp
@@ -0,0 +1,10 @@
+
+#include "stdafx.h"
+#include "task.h"
+
+
+void task_3()
+{
+	cout << "Задание 10.3" << endl;
+	
+}
